149d916947626e7494a8a938cfcee2f00f1f51e6
UttaranHazarika <uhaz.88@gmail.com>
2026-03-13 22:19:59 +0000

    DevelopmentComplete

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.schemas import LoanApplication
 from app.graph.workflow import run_workflow
 
-app=FastAPI(title="AFRICA Agent API")
+app = FastAPI(title="AFRICA Agent API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/assess")
-def assess_credit(application:LoanApplication):
+def assess_credit(application: LoanApplication):
 
     return run_workflow(application.dict())
+
